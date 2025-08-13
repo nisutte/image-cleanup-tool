b9229b49381006707b9bc99c632dbe46f1c30758
nisutte <nicosutter24@gmail.com>
2025-08-13 22:32:45 +0200

    claude haiku api

--- src/image_cleanup_tool/__init__.py
+++ src/image_cleanup_tool/__init__.py
@@ -8,13 +8,16 @@ __version__ = "0.1.0"
 __author__ = "Nico Sutter"
 
 from .core.backbone import ImageScanEngine
-from .api.openai_api import analyze_image, load_and_encode_image
+from .api.openai_api import analyze_image as openai_analyze_image, load_and_encode_image as openai_load_and_encode_image
+from .api.claude_api import analyze_image as claude_analyze_image, load_and_encode_image as claude_load_and_encode_image
 from .core.workers import AsyncWorkerPool, analyze_images_async
 
 __all__ = [
     "ImageScanEngine", 
-    "analyze_image", 
-    "load_and_encode_image",
+    "openai_analyze_image", 
+    "openai_load_and_encode_image",
+    "claude_analyze_image",
+    "claude_load_and_encode_image",
     "AsyncWorkerPool",
     "analyze_images_async"
 ] 
--- src/image_cleanup_tool/api/__init__.py
+++ src/image_cleanup_tool/api/__init__.py
@@ -2,6 +2,12 @@
 API integrations for external services.
 """
 
-from .openai_api import analyze_image, load_and_encode_image
+from .openai_api import analyze_image as openai_analyze_image, load_and_encode_image as openai_load_and_encode_image
+from .claude_api import analyze_image as claude_analyze_image, load_and_encode_image as claude_load_and_encode_image
 
-__all__ = ["analyze_image", "load_and_encode_image"] 
+__all__ = [
+    "openai_analyze_image", 
+    "openai_load_and_encode_image",
+    "claude_analyze_image",
+    "claude_load_and_encode_image"
+] 
